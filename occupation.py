8d1cc71e40e0c8d100d27274baf86dcf5fa64ef7
BermetKalmakova <bermetkalmakova@gmail.com>
2017-09-14 21:09:26 -0400

    done

--- occupation.py
+++ occupation.py
@@ -1,12 +1,38 @@
+import random
 filename = 'occupations.csv'
 f = open(filename,'rU')
 text = f.read()
 
 dict = {}
-L = text.split("\n")
+L1 = text.split("\n")
+L = filter(None, L1)
 i = 1
-while i < len(L):
+count = 0
+while i < len(L)-1:
     if L[i][0] == '"':
-        index = L[i].find('"', beg = 1, end = len(L[i]))
-        dict[float(L[i][index+1:])] = L[i][1:index]
+        index = L[i].find('"', 1, len(L[i]))
+        count += float(L[i][index+2:]) * 10
+        dict[count] = L[i][1:index]
+        i += 1
+    else:
+        line = L[i].split(",")
+        count += float(line[1]) * 10
+        dict[count] = line[0]
+        i += 1
+        
+def rand():
+    ans = 0
+    number = random.randint(1,998)
+    for thing in dict:
+        print thing
+        if ans == 0:
+            if number <= thing:
+                ans = thing
+        elif number <= thing:
+            if thing < ans:
+                ans = thing
+    print dict[ans]
+
+rand()
+
     
